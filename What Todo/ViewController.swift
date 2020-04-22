b8b33f52d671e6c71ef54d14a0d45a20ae7780d2
soma-y1029 <57809819+soma-y1029@users.noreply.github.com>
2020-04-22 16:43:56 -0700

    Initial Commit

---
+++ What Todo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  What Todo
+//
+//  Created by Soma Yoshida on 4/22/20.
+//  Copyright © 2020 What Todo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
